79569822ff7e892d4558420b280e9fde7c1507e3
Jamboy <jamboylive@gmail.com>
2021-07-09 17:03:17 +0800

    购物车子项

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -2,7 +2,7 @@
  * @Description: 购物车
  * @Author: Jamboy
  * @Date: 2021-06-05 11:43:59
- * @LastEditTime: 2021-06-29 10:48:50
+ * @LastEditTime: 2021-07-09 15:41:11
  */
 // pages/cart/cart.js
 import { Cart } from '../../models/cart'
@@ -18,7 +18,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function (options) {
+    
+  },
 
   onShow: function () {
     const cart = new Cart()
