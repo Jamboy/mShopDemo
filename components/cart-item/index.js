5d1e7283a6b40146a113cb5071f8dc101783306a
Jamboy <jamboylive@gmail.com>
2021-07-12 13:54:38 +0800

    购物车子项

--- components/cart-item/index.js
+++ components/cart-item/index.js
@@ -1,23 +1,67 @@
+/*
+ * @Description:
+ * @Author: Jamboy
+ * @Date: 2021-07-08 16:17:03
+ * @LastEditTime: 2021-07-12 11:49:24
+ */
 // components/cart-item/index.js
+import { parseSpecValue } from '../../utils/sku'
+import { Cart } from '../../models/cart'
+const cart = new Cart()
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    cartItem: Object
+    cartItem: Object,
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    specStr: String,
+    discount: Boolean,
+    soldOut: Boolean,
+    online: Boolean,
+    stock: Cart.SKU_MAX_COUNT,
+    skuCount: 1,
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  observers: {
+    cartItem: function (cartItem) {
+      if (!cartItem) {
+        return
+      }
+      const specStr = parseSpecValue(cartItem.sku.specs)
+      const discount = cartItem.sku.discount_price ? true : false
+      const soldOut = Cart.isSoldOut(cartItem)
+      const online = Cart.isOnline(cartItem)
 
-  }
+      this.setData({
+        specStr,
+        discount,
+        soldOut,
+        online,
+        stock: cartItem.sku.stock,
+        skuCount: cartItem.count,
+      })
+    },
+  },
+
+  methods: {
+    onDelete(e) {
+      const { skuId } = this.properties.cartItem
+      cart.removeItem(skuId)
+      this.setData({
+        cartItem: null,
+      })
+      this.triggerEvent('itemdelete', { skuId })
+    },
+
+    checkedItem(e) {
+      const { checked } = e.detail
+      this.properties.cartItem.checked = checked
+      cart.checkItem(this.properties.cartItem.skuId)
+      this.triggerEvent('itemcheck')
+    },
+  },
 })
